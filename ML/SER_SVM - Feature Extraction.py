5bead1d42b46db01fcdc9349008e7e1a9faa7327
manqkuma <x22153209@student.ncirl.ie>
2023-04-28 11:42:50 +0100

    Added config file and speech Reco .py file

---
+++ ML/SER_SVM - Feature Extraction.py
@@ -0,0 +1,6 @@
+import numpy as np
+import pandas as pd
+import librosa
+import glob
+import os
+import time
---
+++ config.py
@@ -0,0 +1,2 @@
+TRAINING_FILES_PATH='/Users/manojkumar/PycharmProjects/Speech-Emotion-Recognition/features/'
+TESS_ORIGINAL_FOLDER_PATH='/Users/manojkumar/PycharmProjects/Speech-Emotion-Recognition/TESS_Toronto_emotional_speech_set_data/'
