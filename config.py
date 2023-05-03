0b110d5c722223733a141a04a707ee13040f6aaa
pavithrasri-udayakumar <pavithrasri.u01@gmail.com>
2023-05-03 01:52:18 +0100

    datasets in feature floder

--- config.py
+++ config.py
@@ -1,2 +1,3 @@
 TRAINING_FILES_PATH='/Users/manojkumar/PycharmProjects/Speech-Emotion-Recognition/features/'
 TESS_ORIGINAL_FOLDER_PATH='/Users/manojkumar/PycharmProjects/Speech-Emotion-Recognition/TESS_Toronto_emotional_speech_set_data/'
+Change in path
