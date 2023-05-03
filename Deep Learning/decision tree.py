ca1807a2ff11cf7a1527da584539d9b629024723
sonifranciz <soniafrancis9899@gmail.com>
2023-05-03 11:56:52 +0100

    cnn

--- Deep Learning/decision tree.py
+++ Deep Learning/decision tree.py
@@ -9,6 +9,7 @@ import numpy as np
 data, sampling_rate = librosa.load(
     r'C:\Users\sonia\Downloads\archive (2)\Audio_Speech_Actors_01-24\Actor_01\03-01-05-02-01-02-01.wav')
 plt.figure(figsize=(12, 4))
+
 # display audio data as wave format
 librosa.display.waveshow(data, sr=sampling_rate)
 #plt.show()
