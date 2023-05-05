aef202ce2afdf9388e27d77edda1d01e204deba5
sonifranciz <soniafrancis9899@gmail.com>
2023-05-05 23:12:01 +0100

    cnn

--- Deep Learning/checkpoint.py
+++ Deep Learning/checkpoint.py
@@ -1,61 +1,67 @@
-# library for analysing signals
+#pip install google-colab
+#from google.colab import drive
+#drive.mount('/content/drive')
+
 import librosa
-# function for visualising audio signals
 from librosa import display
+
+data, sampling_rate = librosa.load('../Datasets/RAVDESS/Song/Actor_04/03-02-06-02-02-02-04.wav')
+
+#% pylab inline
 import matplotlib.pyplot as plt
-import numpy as np
 
-#loads audio as np array and sampling rate
-data, sampling_rate = librosa.load(
-    r'C:\Users\sonia\Downloads\archive (2)\Audio_Speech_Actors_01-24\Actor_01\03-01-05-02-01-02-01.wav')
 plt.figure(figsize=(12, 4))
-
-# display audio data as wave format
 librosa.display.waveshow(data, sr=sampling_rate)
-#plt.show()
 
-# importing libraries
 import time
 import os
-
-path = r'C:\Users\sonia\Downloads\archive (2)\Audio_Speech_Actors_01-24\Actor_06'
+path = '../Datasets/RAVDESS/Song/Actor_04/'
 lst = []
 
 start_time = time.time()
 
+import numpy as np
 for subdir, dirs, files in os.walk(path):
-    for file in files:
-        try:
-            # Load librosa array, obtain mfcss, store the file and the mcss information in a new array
-            X, sample_rate = librosa.load(os.path.join(subdir, file), res_type='kaiser_fast')
-            mfccs = np.mean(librosa.feature.mfcc(y=X, sr=sample_rate, n_mfcc=40).T, axis=0)
-            # The instruction below converts the labels (from 1 to 8) to a series from 0 to 7
-            # This is because our predictor needs to start from 0 otherwise it will try to predict also 0.
-            file = int(file[7:8]) - 1
-            arr = mfccs, file
-            lst.append(arr)
-        # If the file is not valid, skip it
-        except ValueError:
-            continue
+  for file in files:
+      try:
+        #Load librosa array, obtain mfcss, store the file and the mcss information in a new array
+        X, sample_rate = librosa.load(os.path.join(subdir,file), res_type='kaiser_fast')
+        mfccs = np.mean(librosa.feature.mfcc(y=X, sr=sample_rate, n_mfcc=40).T,axis=0)
+        # The instruction below converts the labels (from 1 to 8) to a series from 0 to 7
+        # This is because our predictor needs to start from 0 otherwise it will try to predict also 0.
+        file = int(file[7:8]) - 1
+        arr = mfccs, file
+        lst.append(arr)
+      # If the file is not valid, skip it
+      except ValueError:
+        continue
 
 print("--- Data loaded. Loading time: %s seconds ---" % (time.time() - start_time))
+
+# Creating X and y: zip makes a list of all the first elements, and a list of all the second elements.
+#print(lst)
 X, y = zip(*lst)
+import numpy as np
 X = np.asarray(X)
 y = np.asarray(y)
-print(X.shape)
-print(y.shape)
+
+
+X.shape, y.shape
+# Saving joblib files to not load them again with the loop above
 
 import joblib
+
 X_name = 'X.joblib'
 y_name = 'y.joblib'
-save_dir = r'C:\Users\sonia\Downloads\archive (2)'
+save_dir = '../dataset_features/'
 
 savedX = joblib.dump(X, os.path.join(save_dir, X_name))
 savedy = joblib.dump(y, os.path.join(save_dir, y_name))
-
+# Loading saved models
 import joblib
-X = joblib.load(r'C:\Users\sonia\Downloads\archive (2)\X.joblib')
-y = joblib.load(r'C:\Users\sonia\Downloads\archive (2)\y.joblib')
+X = joblib.load('../dataset_features/X.joblib')
+y = joblib.load('../dataset_features/y.joblib')
+
 from sklearn.model_selection import train_test_split
 
 X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.33, random_state=42)
@@ -64,10 +70,12 @@ dtree = DecisionTreeClassifier()
 dtree.fit(X_train, y_train)
 predictions = dtree.predict(X_test)
 from sklearn.metrics import classification_report,confusion_matrix
-print(classification_report(y_test,predictions,zero_division=1))
+print(classification_report(y_test,predictions))
+
+import numpy as np
 x_traincnn = np.expand_dims(X_train, axis=2)
 x_testcnn = np.expand_dims(X_test, axis=2)
-print(x_traincnn.shape, x_testcnn.shape)
+x_traincnn.shape, x_testcnn.shape
 import keras
 import numpy as np
 import matplotlib.pyplot as plt
@@ -80,10 +88,6 @@ from keras.layers import Input, Flatten, Dropout, Activation
 from keras.layers import Conv1D, MaxPooling1D
 from keras.models import Model
 from keras.callbacks import ModelCheckpoint
-from keras.optimizers import RMSprop
-from tensorflow.keras.models import Sequential
-
-
 
 model = Sequential()
 
@@ -102,13 +106,26 @@ model.add(Dropout(0.1))
 model.add(Flatten())
 model.add(Dense(8))
 model.add(Activation('softmax'))
-opt = RMSprop(learning_rate=0.00005, rho=0.9, epsilon=1e-07, decay=0.0)
+from tensorflow.keras.optimizers import RMSprop
+from tensorflow.keras.optimizers.schedules import ExponentialDecay
+
+initial_learning_rate = 0.00005
+decay_rate = 0.9
+decay_steps = 10000
+
+lr_schedule = ExponentialDecay(
+    initial_learning_rate,
+    decay_steps=decay_steps,
+    decay_rate=decay_rate,
+    staircase=True)
+
+opt = RMSprop(learning_rate=lr_schedule, epsilon=1e-07)
+
 model.summary()
 model.compile(loss='sparse_categorical_crossentropy',
               optimizer=opt,
               metrics=['accuracy'])
 cnnhistory=model.fit(x_traincnn, y_train, batch_size=16, epochs=200, validation_data=(x_testcnn, y_test))
-
 plt.plot(cnnhistory.history['loss'])
 plt.plot(cnnhistory.history['val_loss'])
 plt.title('model loss')
@@ -116,7 +133,6 @@ plt.ylabel('loss')
 plt.xlabel('epoch')
 plt.legend(['train', 'test'], loc='upper left')
 plt.show()
-
 plt.plot(cnnhistory.history['accuracy'])
 plt.plot(cnnhistory.history['val_accuracy'])
 plt.title('model accuracy')
@@ -124,15 +140,16 @@ plt.ylabel('accuracy')
 plt.xlabel('epoch')
 plt.legend(['train', 'test'], loc='upper left')
 plt.show()
+# Use predict method to get predicted class probabilities
+y_pred = model.predict(x_testcnn)
 
-predictions = model.predict_classes(x_testcnn)
+# Get predicted class labels by taking the argmax of the predicted probabilities
+y_pred_classes = y_pred.argmax(axis=-1)
 
 predictions
-
 y_test
 new_Ytest = y_test.astype(int)
 new_Ytest
-
 from sklearn.metrics import classification_report
 report = classification_report(new_Ytest, predictions)
 print(report)
@@ -141,18 +158,9 @@ matrix = confusion_matrix(new_Ytest, predictions)
 print (matrix)
 
 # 0 = neutral, 1 = calm, 2 = happy, 3 = sad, 4 = angry, 5 = fearful, 6 = disgust, 7 = surprised
-
 model.save('testing10_model.h5')
 print("MODEL SAVED")
-
 new_model=keras.models.load_model('testing10_model.h5')
 new_model.summary()
-
 loss, acc = new_model.evaluate(x_testcnn, y_test)
 print("Restored model, accuracy: {:5.2f}%".format(100*acc))
-
-
-
-
-
-
