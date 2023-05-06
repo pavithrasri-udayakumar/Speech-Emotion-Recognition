778b4eb2502afa969e9a8e087e8d3acf3522e8be
sonifranciz <soniafrancis9899@gmail.com>
2023-05-07 00:37:16 +0100

    predicting lively

--- config.py
+++ config.py
@@ -1,3 +1,2 @@
-TRAINING_FILES_PATH=r'C:\Users\sonia\PycharmProjects\Speech-Emotion-Recognition\features'
-TESS_ORIGINAL_FOLDER_PATH=r'C:\Users\sonia\PycharmProjects\Speech-Emotion-Recognition\TESS_Toronto_emotional_speech_set_data'
-SAVE_DIR_PATH=r"C:\Users\sonia\PycharmProjects\Speech-Emotion-Recognition\Deep Learning\features.py"
+TRAINING_FILES_PATH='https://github.com/pavithrasri-udayakumar/Speech-Emotion-Recognition/tree/main/features/'
+TESS_ORIGINAL_FOLDER_PATH='https://github.com/pavithrasri-udayakumar/Speech-Emotion-Recognition/tree/main/TESS_Toronto_emotional_speech_set_data/'
